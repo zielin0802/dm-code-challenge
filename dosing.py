934cedb3118cc7719683982617c0092478999299
Kevin Zielin <kzielin@clearityfoundation.org>
2020-01-26 16:41:09 -0800

    added Dosing class to dosing.py, added unit testing file test_dosing.py

--- dosing.py
+++ dosing.py
@@ -1,39 +1,37 @@
 import os, sys, getopt, pandas
 
-def report_csv(viscode, svdose, ecsdstxt, output_file_dir):
-    ec_input_file_name = "t2_ec 20190619.csv"
-    registry_input_file_name = "t2_registry 20190619.csv"
-    output_file_name = "results.csv"
+class Dosing:
+    def __init__(self, ec_input_file_name, registry_input_file_name, output_file_name):
+        self.output_file_name = output_file_name
+        self.data_ec = self.load_input_file(ec_input_file_name)
+        self.data_registry = self.load_input_file(registry_input_file_name)
 
-    #Read the following files into a dataframe
-    data_ec = pandas.read_csv(ec_input_file_name)
-    data_registry = pandas.read_csv(registry_input_file_name)
+    def load_input_file(self, input_file_name):
+        return pandas.read_csv(input_file_name)
 
-    #Merge the dataframes:
-    merge_result_data = pandas.merge(data_registry[['ID', 'RID', 'USERID', 'VISCODE', 'SVDOSE']], 
-                            data_ec[['ECSDSTXT', 'RID', 'VISCODE']], 
+    def merge(self):
+        return pandas.merge(self.data_registry[['ID', 'RID', 'USERID', 'VISCODE', 'SVDOSE']], 
+                            self.data_ec[['ECSDSTXT', 'RID', 'VISCODE']], 
                             on=['RID', 'VISCODE'], 
                             how='left')
 
-    #Filter records where:
-    merge_result_data = merge_result_data[(merge_result_data.VISCODE == viscode) 
-                                          & (merge_result_data.SVDOSE == svdose) 
-                                          & (merge_result_data.ECSDSTXT != ecsdstxt)]
-    
-    #Create and output a .csv file of the filtered records:
-    try:
-        if (not os.path.isdir(output_file_dir)):
-            os.mkdir(output_file_dir)
-    except:
-        print("Could not create directory " + output_file_dir)
-        sys.exit(2)
-    merge_result_data.to_csv(output_file_dir + "/" + output_file_name, index = None, header = True)
+    def filter(self, data, viscode, svdose, ecsdstxt):
+        return data[(data.VISCODE == viscode) 
+                     & (data.SVDOSE == svdose) 
+                     & (data.ECSDSTXT != ecsdstxt)]
 
-    #print(merge_result_data.head())
+    def to_csv(self, data, output_file_dir):
+        try:
+            if (not os.path.isdir(output_file_dir)):
+                os.mkdir(output_file_dir)
+        except:
+            print("Could not create directory " + output_file_dir)
+            sys.exit(2)
+        data.to_csv(output_file_dir + "/" + self.output_file_name, index = None, header = True)
 
 def main(argv):
-   usage = 'dosing.py -v <viscode> -s <svdose> -e <ecsdstxt> -o <output directory>'
-   viscode, svdose, ecsdstxt, outputfile = '', '', None, ''
+   usage = 'usase: dosing.py -v <viscode> -s <svdose> -e <ecsdstxt> -o <output directory>'
+   viscode, svdose, ecsdstxt, output_file_dir = '', '', None, ''
    try:
       opts, args = getopt.getopt(argv,"hv:s:e:o:",["viscode=", "svdose=", "ecsdstxt=", "odir="])
    except getopt.GetoptError:
@@ -56,7 +54,10 @@ def main(argv):
       elif opt in ("-o", "--odir"):
         output_file_dir = arg
 
-   report_csv(viscode, svdose, ecsdstxt, output_file_dir)
+   dos = Dosing("t2_ec 20190619.csv", "t2_registry 20190619.csv", "results.csv")
+   merge_result_data = dos.merge()
+   filter_result_data = dos.filter(merge_result_data, viscode, svdose, ecsdstxt)
+   dos.to_csv(filter_result_data, output_file_dir)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
---
+++ test_dosing.py
@@ -0,0 +1,49 @@
+﻿import pytest
+from dosing import Dosing
+
+@pytest.fixture
+def dosing():
+    return Dosing("t2_ec 20190619.csv", "t2_registry 20190619.csv", "results.csv")
+
+@pytest.mark.parametrize('''viscode, svdose, ecsdstxt, 
+                            expected_ec_row_count, expected_registry_row_count, expected_merged_row_count, expected_merged_filtered_row_count,
+                            expected_result_col_count, expected_ids, expected_rids, expected_userids, expected_ecsdstxts''', [
+    ('w02', 'Y', 280, 11, 14, 14, 2, 6, [30, 32], [19, 17], ['MORPHEUS_UCSD_EDU_2', 'MORPHEUS_UCSD_EDU'], [-4, 140]),
+    ('w10', 'Y', 280, 11, 14, 14, 0, 6, None, None, None, None)
+])
+def test_merge_filtered(dosing, viscode, svdose, ecsdstxt, 
+                        expected_ec_row_count, expected_registry_row_count, expected_merged_row_count, expected_merged_filtered_row_count,
+                        expected_result_col_count, expected_ids, expected_rids, expected_userids, expected_ecsdstxts):
+    #test init
+    ec_actual_row_count = dosing.data_ec.shape[0]
+    assert ec_actual_row_count == expected_ec_row_count, "ec dataframe contains {0} rows, expecting {1}".format(ec_actual_row_count, expected_ec_row_count)
+
+    registry_actual_row_count = dosing.data_registry.shape[0]
+    assert registry_actual_row_count == expected_registry_row_count, "registry dataframe contains {0} rows, expecting {1}".format(registry_actual_row_count, expected_registry_row_count)
+
+    #test merge
+    merged_data = dosing.merge()
+    merged_actual_row_count = merged_data.shape[0]
+    print(merged_actual_row_count)
+    assert merged_actual_row_count == expected_merged_row_count, "merged dataframe contains {0} rows, expecting {1}".format(merged_actual_row_count, expected_merged_row_count)
+
+    #test merged & filtered
+    #row count
+    merge_filtered_data = dosing.filter(merged_data, viscode, svdose, ecsdstxt)
+    actual_merged_filtered_row_count = merge_filtered_data.shape[0]
+    assert actual_merged_filtered_row_count == expected_merged_filtered_row_count, "merged & filtered dataframe contains {0} rows, expecting {1}".format(merged_filtered_actual_row_count, expected_merged_filtered_row_count)
+    #col count
+    actual_result_col_count = len(merge_filtered_data.columns)
+    assert actual_result_col_count == expected_result_col_count
+    #filters and expected results
+    if (expected_merged_filtered_row_count > 0):
+        for i in range(actual_merged_filtered_row_count):
+            assert merge_filtered_data['VISCODE'].values[i] == viscode
+            assert merge_filtered_data['SVDOSE'].values[i] == svdose
+            assert merge_filtered_data['ECSDSTXT'].values[i] != ecsdstxt
+            assert merge_filtered_data['ID'].values[i] in expected_ids
+            assert merge_filtered_data['RID'].values[i] in expected_rids
+            assert merge_filtered_data['USERID'].values[i] in expected_userids
+            assert merge_filtered_data['ECSDSTXT'].values[i] in expected_ecsdstxts
+            
+
